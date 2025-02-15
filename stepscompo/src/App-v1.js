384ec8c00346140bf2589e276c9cf6bf39432176
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-02-15 17:23:45 +0530

    v2-GenericButton
    Creating and reusing the button and updating the value using children props

--- stepscompo/src/App-v1.js
+++ stepscompo/src/App-v1.js
@@ -44,32 +44,21 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrev}
-              text="Previous"
-              emoji="👈"
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next <span>👉</span>{" "}
-            </Button>
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
