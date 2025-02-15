df5d9e32bdb78f5b025a7ae20ed350daac0f12e6
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-02-15 17:56:32 +0530

    vFinal
    Updated with children props and re-useability and splitting each into component file

--- accordioncomponent/src/App.js
+++ accordioncomponent/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import Accordion from "./Accordion";
 
 const faqs = [
   {
@@ -47,33 +47,6 @@ export default function App() {
   return (
     <div className="App">
       <Accordion data={faqs} />
-    </div>
-  );
-}
-
-function Accordion({ data }) {
-  return (
-    <div className="accordion">
-      {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
-      ))}
-    </div>
-  );
-}
-
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
-  }
-
-  return (
-    <div className={`item ${isOpen ? "open" : "close"}`} onClick={handleToggle}>
-      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
